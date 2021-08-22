bd07da1808863f730d5d6ed1b595ecf196ab18e1
goncrust <66622998+goncrust@users.noreply.github.com>
2021-08-22 21:51:52 +0100

    add comments

--- src/main.py
+++ src/main.py
@@ -24,21 +24,30 @@ def help_page():
     print("\t-o <output file>\tSet destination file name and location.")
 
 def main(args):
+    
+    # Display help
     if (len(args) < 2 or '-h' in args):
         help_page()
+        
+    # Download mode
     elif (args[1] == 'D'):
+        
         qresult = []
+        
+        # Keyword
         if ('-k' in args):
             if ('-s' in args):
                 qresult = query.queryName(args[args.index('-k') + 1], int(args[args.index('-s') + 1]), 2)
             else:    
                 qresult = query.queryName(args[args.index('-k') + 1], 5, 2)
-       
+        
+        # URL
         elif ('-u' in args):
             url = args[args.index('-u') + 1]
             qresult.append(query.queryNameFromURL(url))
             qresult.append(url)
-
+        
+        # Playlist URL
         elif ('-p' in args):
             p_url = args[args.index('-p') + 1]
             qresult = query.queryPlaylistInfo(p_url, 2)
@@ -53,32 +62,35 @@ def main(args):
                 i += 1
 
             return
-
+        
+        # Keyword/URL output file
         if ('-o' in args):
             downloader.downloadYTURL(qresult[1], qresult[0], args[args.index('-o') + 1])
         else:
             downloader.downloadYTURL(qresult[1], qresult[0])
-
+    
+    # Query mode
     elif (args[1] == 'Q'):
+        
+        # Keyword
         if ('-k' in args):
             if ('-s' in args):
                 query.queryName(args[args.index('-k') + 1], int(args[args.index('-s') + 1]), 1)
             else:    
                 query.queryName(args[args.index('-k') + 1], 5, 1)
        
+        # URL
         elif ('-u' in args):
             url = args[args.index('-u') + 1]
             print(query.queryNameFromURL(url))
-
+        
+        # Playlist
         elif ('-p' in args):
             p_url = args[args.index('-p') + 1]
             qresult = query.queryPlaylistInfo(p_url, 1)
-
+    
     else:
         help_page()
 
 
 main(sys.argv)
-
-#qresult = query.queryName(sys.argv[1], 5, True)
-#downloader.downloadYTURL(qresult[1], qresult[0])

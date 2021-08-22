330055239487237436e1bc4d2b9ea9e015838ec3
goncrust <66622998+goncrust@users.noreply.github.com>
2021-08-22 21:42:30 +0100

    add copyright notice

--- src/downloader.py
+++ src/downloader.py
@@ -1,7 +1,19 @@
+'''
+pyMP (https://github.com/goncrust/pyMP)
+
+YouTube audio downloader
+
+Copyright (C) 2021 goncrust
+Released under the GPL v3.0
+https://github.com/goncrust/pyMP/blob/master/LICENSE.md
+'''
+
 import youtube_dl
 import os
 
 def downloadYTURL(url, name, path=None, pl=False):
+    
+    # Set download options
     ydl_opts = {
             'format': 'bestaudio/best',
             'postprocessors': [{
@@ -10,6 +22,7 @@ def downloadYTURL(url, name, path=None, pl=False):
                 'preferredquality': '192', }]
     }
     
+    # Set path
     if (path == None):
         ydl_opts['outtmpl'] = os.getcwd() + f'/{name}.mp3'
     else:
@@ -17,7 +30,8 @@ def downloadYTURL(url, name, path=None, pl=False):
             ydl_opts['outtmpl'] = path 
         else:
             ydl_opts['outtmpl'] = path + f'/{name}.mp3'
-
+    
+    # Download
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.cache.remove()
         ydl.download([url])
